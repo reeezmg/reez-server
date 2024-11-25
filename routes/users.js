9896b004ad1f7cd470172ec06aab51780ebce337
reeezmg <64250221+reeezmg@users.noreply.github.com>
2024-11-25 09:40:26 -0800

    Initial commit

--- routes/users.js
+++ routes/users.js
@@ -74,7 +74,7 @@ router.post('/login', async (req, res) => {
       httpOnly: false,                   // Protect against XSS
       secure: true,                     // Ensure cookie is sent over HTTPS only
       sameSite: 'None',                 // Allow cross-site cookies
-      domain: '.reez.uk',               // Share cookie across subdomains of reez.uk
+            // Share cookie across subdomains of reez.uk
     });
     
 
